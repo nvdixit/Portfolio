35f9bd978b15b2683dbeb4a3085760a1b9508f3f
Nikhil Dixit <nvdixit@ncsu.edu>
2021-05-20 11:13:46 -0500

    Project popups appear in the center of the screen

--- portfolio/src/components/ProjectButton/DescriptionPopup/DescriptionPopup.js
+++ portfolio/src/components/ProjectButton/DescriptionPopup/DescriptionPopup.js
@@ -1,22 +1,28 @@
 import "./DescriptionPopup.css"
 
 function DescriptionPopup(props) {
+    const left = (window.innerWidth - 600) / 2;
+    const top = (window.innerHeight - 600) / 2;
+
     if(props.hasRepo) {
         return (props.trigger) ? (
-            <div id="PopupWindow">
+            <div id="PopupWindow" style={{top: top, left:left}}>
+                <button onClick={() => props.setTrigger(false)} id="closeButton">x</button>
+                <pre/>
                 {props.children}
                 {props.description}
-                <button onClick={() => props.setTrigger(false)}>Close</button><pre/>
-                <a href={props.repoUrl} target="_Blank" rel="noreferrer">Check out the repo!</a>
+                <pre/>
+                <a href={props.repoUrl} target="_Blank" rel="noreferrer" id="repoLink">Check out the repo!</a>
             </div>
         ) : ""
     }
     else {
         return (props.trigger) ? (
-            <div id="PopupWindow">
+            <div id="PopupWindow" style={{top: top, left:left}}>
+                <button onClick={() => props.setTrigger(false)} id="closeButton">x</button>
+                <pre/>
                 {props.children}
                 {props.description}
-                <button onClick={() => props.setTrigger(false)}>Close</button>
             </div>
         ) : ""
     }
