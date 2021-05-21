2d76c0d21cd81ba232e3d0ee2e15ca11eb8984ab
Nikhil Dixit <nvdixit@ncsu.edu>
2021-05-21 16:16:57 -0500

    Linked navbar icon to Contacts page

--- portfolio/src/components/NavigationBar/Navbar.js
+++ portfolio/src/components/NavigationBar/Navbar.js
@@ -8,8 +8,10 @@ function Navbar(props) {
             <li id="navitem"><Link to={props.oneLink} style={{ textDecoration: 'none', color:"inherit" }}>{props.oneName}</Link></li>
             <li id="navitem"><Link to={props.twoLink} style={{ textDecoration: 'none', color:"inherit" }}>{props.twoName}</Link></li>
             <li id="navitem"><Link to={props.threeLink} style={{ textDecoration: 'none', color:"inherit"}}>{props.threeName}</Link></li>
-            <li id="photoIcon">
-                <img id="picture" src={icon} alt="Icon" height="45px"/>
+            <li id="icon">
+                <Link to="/Contact">
+                    <img id="picture" src={icon} alt="Icon" height="45px"/>
+                </Link>
             </li>
         </ul>
     )
