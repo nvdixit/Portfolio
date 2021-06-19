dc1f5eced34512bb0c8a0506aab21e5520863682
Nikhil Dixit <nvdixit@ncsu.edu>
2021-06-19 15:19:12 -0500

    Portfolio website info update

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -23,12 +23,15 @@ function Projects() {
                                                         "Used Alamofire to make HTTP requests to IEXCloud & tickersearch APIs for stock data",
                                                         "Used SwiftyJSON to process data obtained from HTTP requests",
                                                         "Applied CocoaPods dependency manager"]}/>
+
                     <ProjectButton projectName="Portfolio Website" description="My porfolio website" hasRepo={true} language="HTML/CSS/JavaScript"
                                     technicalSummary={["You're on it right now!",
                                                         "Implemented React framework to create resusable and functional UI components (like this popup box)",
                                                         "Gained profeiciency in HTML/CSS/JavaScript",
-                                                        "Used react-router library for multiple webpages"]}
+                                                        "Used react-router library for multiple webpages",
+                                                        "Implemented EmailJS library for Contact Form functionality"]}
                                     repoUrl="https://github.com/nvdixit/Portfolio"/>
+
                     <ProjectButton projectName="PackScheduler" description="A college course registration & management system" hasRepo={false} language="Java"
                                     technicalSummary={["Used Java properties class to create login functionality",
                                                         "Built finite state machine to clean read-in raw data",
